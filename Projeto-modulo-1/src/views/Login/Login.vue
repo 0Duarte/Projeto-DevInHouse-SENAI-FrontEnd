d6a7ff6537314aa0d9bf1f83eddf2247868bc642
0Duarte <guiduartesbs@hotmail.com>
2023-08-29 21:36:44 -0300

    feat: Cria formulario login

---
+++ Projeto-modulo-1/src/views/Login/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-sheet width="300" class="mx-auto">
+    <v-form @submit.prevent>
+      <img src="http://teste" alt="">
+      <v-text-field v-model="email" label="Email"></v-text-field>
+      <v-text-field v-model="password" label="Senha"></v-text-field>
+      <v-btn type="submit" block class="mt-2">Entrar</v-btn>
+      <p>Ainda não tem conta? Cadastre-se</p>
+    </v-form>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+      
+}
+</script>
