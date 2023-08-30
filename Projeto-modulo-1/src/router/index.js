63fff6eeb4b948434a3d1c88347e031d45cd4fd7
0Duarte <guiduartesbs@hotmail.com>
2023-08-30 15:41:18 -0300

    feat: adiciona cadastro e validacao

--- Projeto-modulo-1/src/router/index.js
+++ Projeto-modulo-1/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../views/Login/Login.vue'
 import HomeView from '../views/HomeView.vue'
+import SignUp from '../views/SignUp/SignUp.vue'
 
 
 const router = createRouter({
@@ -15,6 +16,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: Login
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignUp
     }
     
   ]
