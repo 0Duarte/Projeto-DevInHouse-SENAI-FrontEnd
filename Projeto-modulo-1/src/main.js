6ed44b3dc0f9b54955466f1345c82b28b9d26843
0Duarte <guiduartesbs@hotmail.com>
2023-08-29 21:03:09 -0300

    feat: instala e configura o projeto

---
+++ Projeto-modulo-1/src/main.js
@@ -0,0 +1,25 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import '@mdi/font/css/materialdesignicons.css'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+      defaultSet: 'mdi'
+    },
+  })
+
+const app = createApp(App)
+
+.use(vuetify)
+app.use(router)
+app.mount('#app')
