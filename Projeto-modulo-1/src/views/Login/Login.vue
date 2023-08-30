874146ae5a4b01e63b8659076443a3621dbbfb51
0Duarte <guiduartesbs@hotmail.com>
2023-08-29 22:38:20 -0300

    feat: implementa validacao

--- Projeto-modulo-1/src/views/Login/Login.vue
+++ Projeto-modulo-1/src/views/Login/Login.vue
@@ -1,17 +1,78 @@
 <template>
-  <v-sheet width="300" class="mx-auto">
-    <v-form @submit.prevent>
-      <img src="http://teste" alt="">
-      <v-text-field v-model="email" label="Email"></v-text-field>
-      <v-text-field v-model="password" label="Senha"></v-text-field>
-      <v-btn type="submit" block class="mt-2">Entrar</v-btn>
-      <p>Ainda não tem conta? Cadastre-se</p>
-    </v-form>
-  </v-sheet>
+  <div>
+    <v-card class="mx-auto pa-12 pb-8" elevation="8" max-width="448" rounded="lg">
+      <div class="text-subtitle-1 text-medium-emphasis">Email</div>
+
+      <v-text-field v-model="email" density="compact" placeholder="Seu melhor email"
+        prepend-inner-icon="mdi-email-outline" variant="outlined"></v-text-field>
+
+      <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">Senha</div>
+
+      <v-text-field v-model="password" :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+        :type="visible ? 'text' : 'password'" density="compact" placeholder="Coloque sua senha"
+        prepend-inner-icon="mdi-lock-outline" variant="outlined" @click:append-inner="visible = !visible"></v-text-field>
+
+      <v-card class="mb-4">
+        <v-card-text class="text-medium-emphasis text-caption">
+          <div class="d-flex flex-column ">
+            <p class="mb-2">{{this.errors.email}}</p>
+            <p>{{this.errors.password}}</p>
+          </div>
+        </v-card-text>
+      </v-card>
+
+      <v-btn block class="mb-8" color="blue" size="large" variant="tonal" @click="handleLogin">
+        Entrar
+      </v-btn>
+
+      <v-card-text class="text-center">
+        <p>Ainda não tem conta?</p>
+        <a class="text-blue text-decoration-none" href="#" rel="noopener noreferrer" target="_blank">
+          Cadastre-se<v-icon icon="mdi-chevron-right"></v-icon>
+        </a>
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
 
 <script>
+import * as yup from "yup"
+import {captureErrorYup} from '../../utils/captureErrorYup'
+import axios from 'axios'
+
 export default {
-      
+  data() {
+    return {
+      email: "",
+      password: "",
+      errors: []
+    }
+  },
+  methods: {
+
+    handleLogin() {
+      try {
+        const schema = yup.object().shape({
+          email: yup.string().email('Email não é válido').required('Email é obrigatório'),
+          password: yup.string().required("Senha é obrigatória")
+        })
+
+        schema.validateSync(
+          {
+            email: this.email,
+            password: this.password
+          },
+          { abortEarly: false }
+        )
+      } catch (error) {
+        if (error instanceof yup.ValidationError) {
+          console.log(error)
+          // capturar os errors do yup
+          this.errors = captureErrorYup(error)
+          console.log(this.errors)
+        }
+      }
+    }
+  }
 }
 </script>
