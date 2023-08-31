a3b76cc72ba65d852cececaa3a47a5bf7241f87b
0Duarte <guiduartesbs@hotmail.com>
2023-08-30 22:05:18 -0300

    refactor: muda condicao de exibicao do menu

--- Projeto-modulo-1/src/App.vue
+++ Projeto-modulo-1/src/App.vue
@@ -1,29 +1,49 @@
 <template>
   <div>
     <v-layout>
-      <v-app-bar color="red">
+      <v-app-bar v-if="shouldRenderComponent"
+        color="rgb(246, 86, 38)">
         <v-app-bar-title>
           <v-icon>mdi-dumbbell</v-icon>
-          <v-btn><p>Projeto 1</p></v-btn>
+          <v-btn @click="testaRota">
+            <p>Projeto 1</p>
+          </v-btn>
           <router-link to="/"></router-link>
         </v-app-bar-title>
-        
-        <v-btn>
-            <p>Home</p>
-        </v-btn>
-        <v-btn>
-            <p>Alunos</p>
-        </v-btn>
-        <v-btn>
-            <p>Exercícios</p>
-        </v-btn>
-        <v-btn>
-            <p class="mr-2">Sair</p>
-            <v-icon color="#FFF">mdi-logout</v-icon>
-        </v-btn>
 
+        <v-btn>
+          <p>Home</p>
+        </v-btn>
+        <v-btn>
+          <p>Alunos</p>
+        </v-btn>
+        <v-btn>
+          <p>Exercícios</p>
+        </v-btn>
+        <v-btn>
+          <p class="mr-2">Sair</p>
+          <v-icon color="#FFF">mdi-logout</v-icon>
+        </v-btn>
       </v-app-bar>
-      <router-view></router-view>
+      <v-main>
+        <router-view></router-view>
+      </v-main>
     </v-layout>
   </div>
 </template>
+
+<script>
+  export default{
+    methods:{
+      testaRota(){
+        console.log(this.$route.path)
+      }
+    },
+    computed: {
+    shouldRenderComponent() {
+      const excludedRoutes = ['/login', '/signup'];
+      return !excludedRoutes.includes(this.$route.path);
+    }
+  }
+}
+</script>
