bc7db84f1899ef53d5fea1f4f086c1f0c058b199
0Duarte <guiduartesbs@hotmail.com>
2023-09-04 21:17:20 -0300

    feat: adiciona rota dinamica com id

--- Projeto-modulo-1/src/router/index.js
+++ Projeto-modulo-1/src/router/index.js
@@ -53,7 +53,7 @@ const router = createRouter({
       component: WorkoutRegister
     },
     {
-      path: '/workout/view',
+      path: '/workout/view/:id',
       name: 'ViewWorkout',
       component: ViewWorkout
     }
