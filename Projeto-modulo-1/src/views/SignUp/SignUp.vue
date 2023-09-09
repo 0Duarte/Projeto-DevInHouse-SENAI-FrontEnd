f49414f47e2a8516f26dc6723a344b4186ab916f
0Duarte <guiduartesbs@hotmail.com>
2023-09-09 15:48:15 -0300

    feat: add snackbar

--- Projeto-modulo-1/src/views/SignUp/SignUp.vue
+++ Projeto-modulo-1/src/views/SignUp/SignUp.vue
@@ -14,20 +14,21 @@
             <v-text-field v-model="password" type="password" density="compact" placeholder="Coloque sua senha"
                 prepend-inner-icon="mdi-lock-outline" variant="outlined"></v-text-field>
 
-            <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">Confirme a senha</div>
+            <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">Confirme a senha
+            </div>
             <v-text-field v-model="passwordConfirm" type="password" density="compact" placeholder="Confirme sua senha"
                 prepend-inner-icon="mdi-lock-outline" variant="outlined"></v-text-field>
 
             <div density="compact" class="text-subtitle-1 text-medium-emphasis">Selecione o plano</div>
-            <v-select  density="compact" :items="items" item-value="value" variant="outlined" item-title="plan"
+            <v-select density="compact" :items="items" item-value="value" variant="outlined" item-title="plan"
                 v-model="planSelected"></v-select>
 
             <v-card v-if="errors.length != 0" class="mb-4">
                 <v-card-text class="text-medium-emphasis text-caption">
                     <div class="d-flex flex-column">
                         <p>{{ this.errors.name }}</p>
-                        <p>{{ this.errors.email}}</p>
-                        <p >{{ this.errors.password }}</p>
+                        <p>{{ this.errors.email }}</p>
+                        <p>{{ this.errors.password }}</p>
                         <p>{{ this.errors.verifyPassword }}</p>
                         <p>{{ this.errors.planSelected }}</p>
                     </div>
@@ -40,10 +41,13 @@
 
             <v-card-text class="text-center">
                 <p>Já possui uma conta?</p>
-                <a class="text-blue text-decoration-none" href="/login" rel="noopener noreferrer" target="_blank">
-                    Fazer login<v-icon icon="mdi-chevron-right"></v-icon>
-                </a>
+                <router-link to="/" class="text-blue text-decoration-none">Fazer login<v-icon
+                        icon="mdi-chevron-right"></v-icon>
+                </router-link>
             </v-card-text>
+            <v-snackbar v-model="snackbar" timeout=2000 :color="colorSnack" elevation="5" variant="tonal" multi-line>
+                {{ this.snackText }}
+            </v-snackbar>
         </v-card>
     </div>
 </template>
@@ -66,20 +70,25 @@ export default {
                 { plan: 'Prata', value: 'silver' },
                 { plan: 'Ouro', value: 'gold' },
             ],
-            planSelected: "bronze"
+            planSelected: "bronze",
+            snackText: '',
+            snackbar: false,
+            colorSnack: ''
+
         }
     },
     methods: {
         handleSignUp() {
+
             try {
                 const schema = yup.object().shape({
                     name: yup.string().required("Nome completo é obrigatório"),
                     email: yup.string().email('Email não é válido').required('Email é obrigatório'),
                     password:
                         yup.string()
-                        .min(8, 'A senha deve ser maior')
-                        .max(20, 'Deve ter entre 8-20 caracteres')
-                        .required("Senha é obrigatória"),
+                            .min(8, 'A senha deve ser maior')
+                            .max(20, 'Deve ter entre 8-20 caracteres')
+                            .required("Senha é obrigatória"),
                     verifyPassword: yup
                         .string()
                         .required('A confirmação de senhas é necessária')
@@ -99,25 +108,30 @@ export default {
                     { abortEarly: false }
                 )
 
-                    axios({
-                        url:'http://localhost:3000/users',
-                        method: 'POST',
-                        data: {
-                            name: this.name,
-                            email: this.email,
-                            password: this.password,
-                            type_plan: this.planSelected
-                        }
+                axios({
+                    url: 'http://localhost:3000/users',
+                    method: 'POST',
+                    data: {
+                        name: this.name,
+                        email: this.email,
+                        password: this.password,
+                        type_plan: this.planSelected
+                    }
+                })
+
+                    .then(() => {
+                        this.snackText = 'Exercício cadastrado com sucesso, redirecionando...', this.snackbar = true
+                        this.colorSnack = 'success'
+                        setTimeout(() => {
+                            this.$router.push('/');
+                        }, 2000);
+
                     })
-                        .then(()=> {
-                            alert('Cadastrado com sucesso')
-                            this.$router.push('/login')
-                        })
-                        .catch(()=>{
-                            alert("Não foi possível criar a conta nesse momento")
-                        })
-                    
-                    debugger
+                    .catch(() => {
+                        console.log("Não foi possível criar a conta nesse momento")
+                    })
+
+
 
             } catch (error) {
                 if (error instanceof yup.ValidationError) {
